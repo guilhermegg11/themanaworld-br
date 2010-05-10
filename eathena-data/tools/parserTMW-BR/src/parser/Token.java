91ea647cfba713a22a721d6377678600268e936a
diogorbg <diogorbg@1a48e800-40c6-11de-b5d3-5d1980107c5c>
2010-05-10 20:57:35 +0000

    tools. Iniciei a construção de uma ferramenta que irá nos ajudar em vários aspéctos... podendo até fazer parte do TMW-MAKER futuramente.
    Por enquanto só tenho isso e + algumas classes:
    
    /**
     * 
     * Este programa pode ser utilizado para gerar uma lista de itens, monstros, informações sobre jogadores
     * e outras coisas mais complexas como criar um histórico de tudo que acontece no jogo. Por enquanto só
     * estou trabalhando nas classes.
     * Estando feitas as classe qualquer um poderá dar sua contribuição... fazendo uma classe que explora
     * um aspécto X nos dados do jogo.
     * 
     * @data 10/05/2010
     * @author Diogo_RBG - http://diogorbg.blogspot.com/
     * 
     */
    
    public class Conversor {
    
    	public static void main (String args[]) {
    		System.out.println("123... testando...");
    	}
    
    }

---
+++ eathena-data/tools/parserTMW-BR/src/parser/Token.java
@@ -0,0 +1,46 @@
+/**
+ * Define o objeto Token. Utilizado pela classe Script.
+ * 
+ * @data 10/05/2010
+ * @author Diogo_RBG - http://diogorbg.blogspot.com/
+ * 
+ * Wikipédia: Token em computação é um segmento de texto ou símbolo que pode ser manipulado por
+ * um parser, que fornece um significado ao texto; em outras palavras, é um conjunto de caracteres
+ * (de um alfabeto, por exemplo) com um significado coletivo.
+ */
+
+package parser;
+
+enum TipoToken{
+	CMD, // Comando
+	TXT, // Texto
+	VLR, // Valor
+	VAR  // Variável ou constante
+}
+
+public class Token {
+	private TipoToken tipo;
+	private String valor;
+
+	public Token(TipoToken _tipo, String _valor) {
+		tipo = _tipo;
+		valor = _valor;
+	}
+
+	/**
+	 * O tipo de token. Que pode ser comando, texto, valor ou variável/constante.
+	 * @return Um objeto do tipo TipoToken.
+	 */
+	public TipoToken getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * Retorna o valor do token. Tokens do tipo TXT não possuem aspas duplas >> " << e nem caracteres de escape >> \ <<.
+	 * @return Uma string contendo o token em si.
+	 */
+	public String getValor() {
+		return valor;
+	}
+
+}
