cad78f673810c8cf72ef57acc6a5ec027ff8790c
thiago@themanaworld.com.br <thiago@themanaworld.com.br@1a48e800-40c6-11de-b5d3-5d1980107c5c>
2010-06-21 16:12:19 +0000

    Adicionada nova constante EA_ACCOUNT_TXT apontando para o caminho do arquivo account.txt no eAthena

--- tmwwebadmin/defs.php
+++ tmwwebadmin/defs.php
@@ -6,6 +6,8 @@ define('LADMIN_HOST', '127.0.0.1');
 define('LADMIN_PORT', 6901);
 define('LADMIN_PASS', 'admin');
 
+define('EA_ACCOUNT_TXT', '/home/eathena/eathena/save/account.txt');
+
 define('MYSQL_HOST', 'localhost');
 define('MYSQL_PORT', 3306);
 define('MYSQL_USER', 'root');
