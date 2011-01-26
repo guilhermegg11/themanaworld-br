8b8baf4ea3973c1587d4636b5296b4ba9de36bd4
diogorbg <diogorbg@1a48e800-40c6-11de-b5d3-5d1980107c5c>
2011-01-26 18:41:19 +0000

    Melhorando a ferramenta que analisa o arquivo de itens do eAthena.

--- trunk/eathena-data/tools/parserTMW-BR/src/parser/Item.java
+++ trunk/eathena-data/tools/parserTMW-BR/src/parser/Item.java
@@ -7,6 +7,8 @@
 
 package parser;
 
+import parser.ParserItens.ColItem;
+
 public class Item {
 
 	public enum TipoCol{
@@ -71,6 +73,50 @@ public class Item {
 
 	public Object[] getCols() {
 		return cols;
+	}
+
+	public String getId(){
+		return getColString( ColItem.ID.ordinal() );
+	}
+
+	public String getNome(){
+		return getColString( ColItem.NOME.ordinal() );
+	}
+
+	public String getDesc(){
+		return getColString( ColItem.DESC.ordinal() );
+	}
+
+	public String getTipo(){
+		return getColString( ColItem.TIPO.ordinal() );
+	}
+
+	public String getCompra(){
+		return getColString( ColItem.COMP.ordinal() );
+	}
+
+	public String getVenda(){
+		return getColString( ColItem.VEND.ordinal() );
+	}
+
+	public String getPeso(){
+		return getColString( ColItem.PESO.ordinal() );
+	}
+
+	public String getAtaque(){
+		return getColString( ColItem.ATK.ordinal() );
+	}
+
+	public String getDefesa(){
+		return getColString( ColItem.DEF.ordinal() );
+	}
+
+	public String getAlcance(){
+		return getColString( ColItem.RANGE.ordinal() );
+	}
+
+	public String getMBonus(){
+		return getColString( ColItem.MBONUS.ordinal() );
 	}
 
 }
