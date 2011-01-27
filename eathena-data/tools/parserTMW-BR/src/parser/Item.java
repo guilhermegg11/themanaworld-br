aa531658c4d24d25394644326ac9be220c07f5a6
diogorbg <diogorbg@1a48e800-40c6-11de-b5d3-5d1980107c5c>
2011-01-27 17:48:51 +0000

    Melhorando a ferramenta que analisa o arquivo de itens do eAthena.  - Vejam o arquivo itensAtivos.xml... nele poderemos agrupar os itens a nosso critério e ainda dar mais detalhes como: Se foi desenvolvido pelo tmwbr, quem foram os artistas e em que data. Como é possível obtê-lo (vendido, quest, evento, especial (itens de admins podemos ocultar) )

--- eathena-data/tools/parserTMW-BR/src/parser/Item.java
+++ eathena-data/tools/parserTMW-BR/src/parser/Item.java
@@ -1,25 +1,42 @@
 /**
- * 
  * @date 12/05/2010
  * @author Diogo_RBG - http://diogorbg.blogspot.com/
- * 
  */
 
 package parser;
 
-import parser.ParserItens.ColItem;
-
 public class Item {
 
-	public enum TipoCol{
+	//#ID,Name,Label,Type,Price,Sell,Weight,ATK,DEF,Range,Mbonus,Slot,Gender,Loc,wLV,eLV,View,{UseScript},{EquipScript}
+	public static final int ID =     0;  //< Id do item
+	public static final int NOME =   1;  //< Nome do item
+	public static final int DESC =   2;  //< Descricão do item
+	public static final int TIPO =   3;  //< Tipo do item
+	public static final int COMP =   4;  //< Valor de compra
+	public static final int VEND =   5;  //< Valor de venda
+	public static final int PESO =   6;  //< Peso do item
+	public static final int ATK =    7;  //< Pontos de ataque
+	public static final int DEF =    8;  //< Pontos de defese
+	public static final int RANGE =  9;  //< Distância de ataque
+	public static final int MBONUS = 10; //< Bonus de Magia
+	public static final int SLOT =   11; //< Itens por slot
+	public static final int GENERO = 12; //< Gênero do item
+	public static final int LOC =    13; //< ?
+	public static final int WLV =    14; //< ?
+	public static final int ELV =    15; //< ?
+	public static final int VIEW =   16; //< ?
+	public static final int USO =    17; //< Script de uso
+	public static final int EQP =    18; //< Script de equip
+
+	public enum TipoCol {
 		VALOR,  // Valor. Objeto do tipo String.
 		SCRIPT  // Script. Objeto do tipo Script.
 	}
 
 	private Object[] cols = null;
 
-	public Item(Object[] _cols) {
-		cols = _cols;
+	public Item(Object[] cols) {
+		this.cols = cols;
 	}
 
 	/**
@@ -27,12 +44,12 @@ public class Item {
 	 * @param col Índice da coluna desejada.
 	 * @return Retorna null caso não seja de nenhum tipo. ¬¬ neste caso é porque tá bugado !
 	 */
-	public TipoCol getColTipo(int col){
+	public TipoCol getColTipo(int col) {
 		Object obj = cols[col];
 
-		if(obj instanceof String){
+		if(obj instanceof String) {
 			return TipoCol.VALOR;
-		}else if(obj instanceof Script){
+		} else if(obj instanceof Script) {
 			return TipoCol.SCRIPT;
 		}
 		return null;
@@ -43,7 +60,7 @@ public class Item {
 	 * @param col Índice da coluna desejada.
 	 * @return Retorna null caso o objeto da coluna não seja do tipo String.
 	 */
-	public String getColString(int col){
+	public String getColString(int col) {
 		Object obj = cols[col];
 
 		if(obj instanceof String)
@@ -56,7 +73,7 @@ public class Item {
 	 * @param col Índice da coluna desejada.
 	 * @return Retorna null caso o objeto da coluna não seja do tipo Script.
 	 */
-	public Script getColScript(int col){
+	public Script getColScript(int col) {
 		Object obj = cols[col];
 
 		if(obj instanceof Script)
@@ -67,7 +84,7 @@ public class Item {
 	/**
 	 * Retorna a quantidade de colunas. Também pode ser ulilizado obj.getCols().length
 	 */
-	public int cont(){
+	public int cont() {
 		return cols.length;
 	}
 
@@ -75,48 +92,80 @@ public class Item {
 		return cols;
 	}
 
-	public String getId(){
-		return getColString( ColItem.ID.ordinal() );
+	public String getId() {
+		return getColString(ID);
 	}
 
-	public String getNome(){
-		return getColString( ColItem.NOME.ordinal() );
+	public String getNome() {
+		return getColString(NOME);
 	}
 
-	public String getDesc(){
-		return getColString( ColItem.DESC.ordinal() );
+	public String getDesc() {
+		return getColString(DESC);
 	}
 
-	public String getTipo(){
-		return getColString( ColItem.TIPO.ordinal() );
+	public String getTipo() {
+		return getColString(TIPO);
 	}
 
-	public String getCompra(){
-		return getColString( ColItem.COMP.ordinal() );
+	public String getCompra() {
+		return getColString(COMP);
 	}
 
-	public String getVenda(){
-		return getColString( ColItem.VEND.ordinal() );
+	public String getVenda() {
+		return getColString(VEND);
 	}
 
-	public String getPeso(){
-		return getColString( ColItem.PESO.ordinal() );
+	public String getPeso() {
+		return getColString(PESO);
 	}
 
-	public String getAtaque(){
-		return getColString( ColItem.ATK.ordinal() );
+	public String getAtaque() {
+		return getColString(ATK);
 	}
 
-	public String getDefesa(){
-		return getColString( ColItem.DEF.ordinal() );
+	public String getDefesa() {
+		return getColString(DEF);
 	}
 
-	public String getAlcance(){
-		return getColString( ColItem.RANGE.ordinal() );
+	public String getAlcance() {
+		return getColString(RANGE);
 	}
 
-	public String getMBonus(){
-		return getColString( ColItem.MBONUS.ordinal() );
+	public String getMBonus() {
+		return getColString(MBONUS);
+	}
+
+	public String getSlot() {
+		return getColString(SLOT);
+	}
+
+	public String getGenero() {
+		return getColString(GENERO);
+	}
+
+	public String getLoc() {
+		return getColString(LOC);
+	}
+
+	public String getWLV() {
+		return getColString(WLV);
+	}
+
+	public String getELV() {
+		return getColString(ELV);
+	}
+
+	public String getView() {
+		return getColString(VIEW);
+	}
+
+	public Script getUso() {
+		return getColScript(USO);
+	}
+
+	public Script getEquip() {
+		return getColScript(EQP);
 	}
 
 }
