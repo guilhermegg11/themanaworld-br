4533a9319103fed05e9e0ed85b927394784b4ddd
rui.gravata@gmail.com <rui.gravata@gmail.com@1a48e800-40c6-11de-b5d3-5d1980107c5c>
2013-07-07 04:28:42 +0000

    * Tentativa de adicionar ondas no mapa "007.tmx) coordenadas (33,80)  * Adicionando alguns Spawn ao Colisel.

--- trunk/tmwdata/graphics/tiles/water-anime-raging.tsx
+++ trunk/tmwdata/graphics/tiles/water-anime-raging.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Water" tilewidth="32" tileheight="32">
- <image source="water.png" width="480" height="352"/>
+ <image source="water-anime.png" width="480" height="352"/>
  <tile id="120">
   <properties>
    <property name="animation-delay0" value="18"/>
